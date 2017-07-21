80d2ca5b947d05093022e636603e487136a001eb
Nabil Hassein <nabil@khanacademy.org>
2017-07-21 18:29:17 -0400

    renamings, other WIP changes

--- rhymes.js
+++ rhymes.js
@@ -1,14 +1,6 @@
-function getSongs() {
-    songs = {}
-
-    directories = [path.join(data_dir, d) for d in os.listdir(data_dir)]
-
-    return {song: open(os.path.join(d, song)).read()
-            for d in directories for song in os.listdir(d)}
-}
-
-function getUniqueWords(songs) {
-}
+var songs = [
+    
+]
 
 function getRhymingPair(words) {
     function randomItem(arr) {
